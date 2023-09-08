5bbc1e5abe8e456ffe5d5089865352345dca1bbc
wakana <wakanawardrobe@gmail.com>
2023-09-08 15:23:15 +0100

    add brand page where user can delete a brand

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,4 +1,5 @@
 const Brand = require("../models/Brand");
+const Synthesizer = require("../models/Synthesizer");
 const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 
@@ -14,6 +15,51 @@ const brandListGet = asyncHandler( async(req, res, next) => {
     });
 });
 
+const brandDetailGet = asyncHandler( async (req, res, next) => {
+
+    //render an individual brand page along with the list of all of the synthesizers that are under that brand.
+
+    const { brandName } = req.params;
+
+    const formattedName = brandName[0].toUpperCase() + brandName.slice(1);
+
+    const foundBrand = await Brand.findOne({ name: formattedName }).exec();
+
+    if (!foundBrand) {
+        const err = new Error("Brand not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    const allSynthsByBrand = await Synthesizer.find({ brand: foundBrand._id }).exec();
+
+    res.render("brand-page", {
+        title: "Sergio's Synthesizer Store",
+        brand: foundBrand,
+        synthesizers: allSynthsByBrand        
+    });
+});
+
+const brandDeletePost = asyncHandler( async (req, res, next) => {
+
+    //we need to get the brand, and we need to get all the synths which have this brand listed. Then we need to check if the synth list for this brand is empty, if its empty we can go ahead and delete it. If its not empty we don't want to remove it.
+
+    const { brandName } = req.params;
+
+    const formattedName = brandName[0].toUpperCase() + brandName.slice(1);
+    const foundBrand = await Brand.findOne({ name: formattedName }).exec();
+
+    const allSynthsByBrand = await Synthesizer.find({ brand: foundBrand._id }).exec();
+
+    if (allSynthsByBrand.length > 0) {
+        res.send('THERE ARE SYNTHS THAT ALREADY EXIST WITH THIS BRAND')
+    }
+
+
+});
+
 module.exports = {
-    brandListGet
+    brandListGet,
+    brandDetailGet,
+    brandDeletePost
 }
--- routes/synthesizers.js
+++ routes/synthesizers.js
@@ -10,7 +10,11 @@ const {
     synthDeletePost
 } = require("../controllers/synthController");
 
-const { brandListGet } = require("../controllers/brandController");
+const { 
+    brandListGet,
+    brandDetailGet,
+    brandDeletePost
+} = require("../controllers/brandController");
 
 router.get("/", getSynthList);
 
@@ -29,12 +33,13 @@ router.get("/:synthId/delete", synthDeleteGet);
 
 router.post("/:synthId/delete", synthDeletePost);
 
-// router.get("/:synthId/update");
-
 // we want to be able to have a seperate route that displays us the synthesizers by the brand name. f.ex /brand/exampleBrandName
 
 //BRANDS
 
+router.get("/brands/:brandName", brandDetailGet);
+
+router.post("/brands/:brandName/delete", brandDeletePost);
 
 // router.get("/brands/create");
 
